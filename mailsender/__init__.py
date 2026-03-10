d1943f450b371320bcdbe9deee0318c696f6ce07
Benjamin Edwards <bedw@bedw.me>
2026-03-10 08:35:14 -0300

    better access to relevant classes while importing

--- mailsender/__init__.py
+++ mailsender/__init__.py
@@ -1,5 +1,10 @@
 from .lib.message import Attachment, Message
 from .lib.sender import Sender
-from .settings import config
-
-__all__ = [Message, Attachment, Sender, config]
+from .db.accounts import Account, SQLiteAccountsDBInterface
+from .db.records import Record, PgRecordsDBInterface
+from .settings import (
+    Settings,
+    SenderSettings,
+    GmailSettings,
+    DBSettings,
+)
