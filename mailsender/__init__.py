7adb6d860b652b1dae71daa6af1843f61d4c8a8b
Benjamin Edwards <bedw@bedw.me>
2026-09-22 20:00:50 -0300

    export ses objects

--- mailsender/__init__.py
+++ mailsender/__init__.py
@@ -1,10 +1,12 @@
 from .lib.message import Attachment, Message
 from .lib.sender import Sender
+from .lib.awsses import AmazonSESService
 from .db.accounts import Account, SQLiteAccountsDBInterface
 from .db.records import Record, PgRecordsDBInterface
 from .settings import (
     Settings,
     SenderSettings,
     GmailSettings,
+    AWSCredentials,
     DBSettings,
 )
